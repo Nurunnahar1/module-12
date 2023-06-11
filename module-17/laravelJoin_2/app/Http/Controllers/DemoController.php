7b717b4ae42371a90986c94d6a2fc32e7fda7489
Nurunnahar <nurunnahar9081@gmail.com>
2023-06-11 12:14:37 +0600

    module 17

--- module-17/laravelJoin_2/app/Http/Controllers/DemoController.php
+++ module-17/laravelJoin_2/app/Http/Controllers/DemoController.php
@@ -8,10 +8,6 @@ use Illuminate\Support\Facades\DB;
 class DemoController extends Controller
 {
   function DemoAction(){
-    //===========answer to the question no 4===================
-    // $posts = DB::table('posts')->where('id', 2)->first();
-    // return $posts;
-
 
     //===========answer to the question no 2===================
     $posts = DB::table('posts')
@@ -19,60 +15,65 @@ class DemoController extends Controller
     ->get();
 
     return($posts);
+    //===========answer to the question no 4===================
+    $posts = DB::table('posts')->where('id', 2)->first();
+    return $posts;
+
+
       //===========answer to the question no 5===================
-    // $posts = DB::table('posts')->where('id', 2)->pluck('description')->first();
-    // return $posts;
+    $posts = DB::table('posts')->where('id', 2)->pluck('description')->first();
+    return $posts;
 
 
       //===========answer to the question no 7===================
-    //   $posts = DB::table('posts')->pluck('title');
-    //   return $posts;
+      $posts = DB::table('posts')->pluck('title');
+      return $posts;
 
 
       //===========answer to the question no 8===================
-        // $result=DB::table('posts')->insert([
+        $result=DB::table('posts')->insert([
 
-        //     'title' => 'X',
-        //     'slug' => 'X',
-        //     'excerpt' => 'excerpt',
-        //     'description' => 'description',
-        //     'is_published' => true,
-        //     'min_to_read' => 2
-        // ]);
-        // return $result;
- 
+            'title' => 'X',
+            'slug' => 'X',
+            'excerpt' => 'excerpt',
+            'description' => 'description',
+            'is_published' => true,
+            'min_to_read' => 2
+        ]);
+        return $result;
+
 
 
       //===========answer to the question no 9===================
-    //   $data = [
-    //     'excerpt' => 'Laravel 10',
-    //     'description' => 'Laravel 10'
-    // ];
+      $data = [
+        'excerpt' => 'Laravel 10',
+        'description' => 'Laravel 10'
+    ];
 
-    // $data_1 = DB::table('posts')->where('id', 2)->update($data);
-    // return $data_1;
+    $data_1 = DB::table('posts')->where('id', 2)->update($data);
+    return $data_1;
 
 
       //===========answer to the question no 10===================
-    //   $data = DB::table('posts')->where('id', 3)->delete();
-    //   return $data;
+      $data = DB::table('posts')->where('id', 3)->delete();
+      return $data;
 
 
 
       //===========answer to the question no 14===================
-    //   $posts = DB::table('posts')
-    //   ->whereBetween('min_to_read', [1, 5])
-    //   ->get();
+      $posts = DB::table('posts')
+      ->whereBetween('min_to_read', [1, 5])
+      ->get();
 
-    //     return($posts);
+        return($posts);
 
 
 
       //===========answer to the question no 15===================
-    //   $data = DB::table('posts')
-    //   ->where('id', 3)
-    //   ->increment('min_to_read', 1);
+      $data = DB::table('posts')
+      ->where('id', 3)
+      ->increment('min_to_read', 1);
 
-    //     return $data;
+        return $data;
   }
 }
